c4f7f6b2c346381604ab34eced144aac8e772d08
AlekseyPanas <panasaleksey3@gmail.com>
2021-03-08 05:23:24 -0500

    move session secret

--- Shopifyemroses/app.js
+++ Shopifyemroses/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: "Eccola cosa che non puoi vedere",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }));
